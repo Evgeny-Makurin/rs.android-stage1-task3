dcc93071a6bf96bc235b889f1ed0005e350336b6
Evgeny-Makurin <59473185+Evgeny-Makurin@users.noreply.github.com>
2020-05-07 08:35:25 +0300

    rs.android-stage1-task3

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -1,9 +1,28 @@
 package subtask2
 
 class Combinator {
-
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
+        val m = array[0] //number of posters
+        val n = array[1] //available colors
+        var x = 0
+        var combinationsNumber = 1
+
+        fun factorial(number: Int): Long{
+            var numberFactorial = 1L
+            for (i in 1..number) numberFactorial*=i
+            return numberFactorial
+        }
+
+        while (combinationsNumber < m && combinationsNumber != 0){
+            x++
+            combinationsNumber = (factorial(n) / (factorial(n - x) * factorial(x))).toInt()
+            println("cn "+ combinationsNumber)
+        }
+
+        if (combinationsNumber == m) return x
+        else return null
         throw NotImplementedError("Not implemented")
     }
 }
+
